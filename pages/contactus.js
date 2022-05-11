65c511ce66ae201208f022bf50a3ad1c142e6e19
pushpak90 <64779040+pushpak90@users.noreply.github.com>
2022-05-11 19:38:02 +0530

    Wed
    11/May

--- pages/contactus.js
+++ pages/contactus.js
@@ -1,27 +1,82 @@
 import link from "next/link";
+
+
+import { userUser, useUser } from "@auth0/nextjs-auth0"
 import styles from "../styles/contactus.module.css";
 const contactus = () => {
+  const { user, error, loading } = useUser();
   return (
-    <main>
-      <div class="section">
-        <div className={styles.navbar}>
-          <nav>
-            <div class="logo">
-              <img src="/logo.png" />
+    <main className="{style.bgimg}">
+      <div >
+        {/* <div className={styles.slide}></div> */}
+
+        <div className={styles.bgimg}>
+          <div className={styles.navbar}>
+            <nav>
+              <div class="logo">
+                <img src="/logo.png" />
+              </div>
+              <ul>
+                <li>
+                  <a href="/home" class="nav-link">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="/aboutus" class="nav-link">
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a href="/contactus" class="nav-link">
+                    Contact Us
+                  </a>
+                </li>
+                <li>
+                  <a href="/recipe" class="nav-link">
+                    Recipe
+                  </a>
+                </li>
+                <li>
+                  {user && (
+                    <a href="/api/auth/logout" class="nav-link">
+                      Logout
+                    </a>
+                  )}
+                  {!user && (
+                    <a href="/api/auth/login" class="nav-link">
+                      Login
+                    </a>
+                  )}
+                </li>
+                {/* <li>
+                  <a href="/signup" class="nav-link">
+                    Sign Up
+                  </a>
+                </li> */}
+                <li>
+                  {user && (
+                    <img src={user.picture} className={styles.userImage} />
+                  )}
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className={styles.outter}>
+            <div className={styles.forms}>
+              <input type="text" name="name"  placeholder="Enter Your Name" required></input>
+
             </div>
-            <ul>
-              <li>
-                <a href="/home" className="nav-link">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="/aboutus" className="nav-link">
-                  About
-                </a>
-              </li>
-            </ul>
-          </nav>
+            <div className={styles.forms}>
+              <input type="text" name="name"  placeholder="Enter Email Address" required></input>
+
+            </div>
+            <div className={styles.forms}>
+              <input type="text" name="name"  placeholder="Enter Your subject" required></input>
+            </div>
+            <textarea  name="message" className="message" placeholder="Message" required></textarea>
+            <button type="submit" >Send Message</button>
+          </div>
         </div>
       </div>
     </main>
