e96cebdc746ffa51f73e96fabf5720c41b32dc92
Suchita Mishra <suchitamishra318@gmail.com>
2022-07-07 02:33:34 +0530

    Update aboutus.js

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/aboutus.module.css";
 const aboutus = () => {
+  const { user } = useUser();
   return (
     <main>
       <div>
