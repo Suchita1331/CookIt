410486682dc778fa89f5485d1e2dd1a3a8508dfb
Suchita Mishra <suchitamishra318@gmail.com>
2022-07-07 00:26:34 +0530

    Update members.js

--- pages/members.js
+++ pages/members.js
@@ -1,87 +1,102 @@
 import { useUser } from "@auth0/nextjs-auth0";
-import link from "next/link";
+import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/members.module.css";
 const members = () => {
   const { user } = useUser();
   return (
     <main>
-      <div class="section">
+      <div className="section">
         <div className={styles.navbar}>
           <nav>
             <div class="logo">
-              <img src="/logo1.png" />
+              <Image src="/logo1.png" />
             </div>
             <ul>
               <li>
-                <a href="/" className="nav-link">
-                  Home
-                </a>
+                <Link href="/" className="nav-link">
+                  <a>
+                    Home
+                  </a>
+                </Link>
               </li>
               <li>
-                <a href="/contactus" className="nav-link">
-                  Contact Us
-                </a>
+                <Link href="/contactus" className="nav-link">
+                  <a>
+                    Contact Us
+                  </a>
+                </Link>
               </li>
               <li>
-                <a href="/recipe" class="nav-link">
-                  Recipe
-                </a>
+                <Link href="/recipe" className="nav-link">
+                  <a>
+                    Recipe
+                  </a>
+                </Link>
               </li>
               <li>
                 {user && (
-                  <a href="/api/auth/logout" class="nav-link">
-                    Logout
-                  </a>
+                  <Link href="/api/auth/logout" className="nav-link">
+                    <a>
+                      Logout
+                    </a>
+                 </Link>
                 )}
                 {!user && (
-                  <a href="/api/auth/login" class="nav-link">
-                    Login
-                  </a>
+                  <Link href="/api/auth/login" className="nav-link">
+                    <a>
+                      Login
+                    </a>
+                  </Link>
                 )}
               </li>
             </ul>
           </nav>
         </div>
       </div>
-      <div class="title">
+      <div className="title">
         <div className={styles.body}>
           <section>
             <div classname={styles.image}>
-              <img src="https://pngimg.com/uploads/chef/chef_PNG132.png" />
+              <Image src="https://pngimg.com/uploads/chef/chef_PNG132.png" />
             </div>
             <div>
               <h2> Members </h2>
               <span></span>
               <div>
-                <img src=".." />
+                <Image src=".." />
                 <h3>Ms. Suchita Mishra</h3>
                 <p>Dept - Computer Science And Engineering</p>
                 <p>Roll No. CS19B38</p>
               </div>
               <div>
-                <img src="../images/Sonu.jpg" />
+                <Image src="../images/Sonu.jpg" />
                 <h3>Ms. Shubhangi Chaudhary</h3>
                 <p>Dept - Computer Science And Engineering</p>
                 <p>Roll No. CS19B34</p>
               </div>
               <div>
-                <img src="#" />
+                <Image src="#" />
                 <h3>Mr. Pushpak Fasate</h3>
                 <p>Dept - Computer Science And Engineering</p>
                 <p>Roll No. CS19B58</p>
               </div>
               <div>
-                <img src="#" />
+                <Image src="#" />
                 <h3>Mr. Sushant Choudhari</h3>
                 <p>Dept - Computer Science And Engineering</p>
                 <p>Roll No. CS19B65</p>
               </div>
               <ul className={styles.links}>
                 <li>
-                  <a href="/aboutus"> About us </a>
+                  <Link href="/aboutus">
+                    <a> About us </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="/contactus"> Contact us </a>
+                  <Link href="/contactus">
+                    <a> Contact us </a>
+                  </Link>
                 </li>
               </ul>
             </div>
