dda33b6dd4e809aad069d69c1b4378aeb44df43e
Suchita Mishra <suchitamishra318@gmail.com>
2022-07-07 02:32:50 +0530

    Update members.js

--- pages/members.js
+++ pages/members.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/members.module.css";
 const members = () => {
+  const { user } = useUser();
   return (
     <main>
       <div>
